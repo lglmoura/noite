b892ca446eb2eee50efd4dae94b12637ad5b7a64
Luiz Gustavo <lglmoura@gmail.com>
2013-04-01 19:53:49 -0300

    Inicio Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noite::Application.config.secret_token = 'f9b79cb010075e71036902e5b1aa5c3f2a73c2a73a9e30ccbec71ab873992a3ce533ce732c27367772a8a078e15802ca4d5e063b72269c19e968e7b1877b3e35'
